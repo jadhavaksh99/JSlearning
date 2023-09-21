28c0d104aa37befdc607427fbdd9a09f1af712ba
jadhavaksh99 <jadhavaksh99@gmail.com>
2023-09-21 07:30:28 +0530

    designed variable
    designed variable

---
+++ 01_variable.js
@@ -0,0 +1,23 @@
+
+var myFullName //variable declaration
+myFullName = "Akshay Jadhav" //variable initialization
+
+var myFullName = "Akshay Dagdusaheb Jadhav"
+console.log(myFullName);
+
+var myLove = "I love only JS"
+var firstName = "akshay"
+var lastName = "jadhav"
+var age = "29"
+var address = "pune"
+var pinCode = 411028
+var gender = "m"
+var isMarried = "yes"
+console.log(myLove);
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pinCode);
+console.log(gender);
+console.log(isMarried);
