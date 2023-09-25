50c7cc7a359879751eb65afe732de6bf16ee5cb4
jadhavaksh99 <jadhavaksh99@gmail.com>
2023-09-25 23:09:09 +0530

    designed string assignment
    designed string assignment

---
+++ 03_funAssign01.js
@@ -0,0 +1,8 @@
+function bankDetails(bankName, accountNum, location, pinCode){
+    console.log('My first bank Details are :  ', 'bankname -',bankName, ', A/C no - ', accountNum, 'in', location, 'pin code', pinCode);
+}
+bankDetails("HDFC", 54654651, "Pune", 14553);
+bankDetails("SBI", 548495314, "Pune", 51356);
+bankDetails("ICICC", 8652131125, "Pune", 984663);
+
+
---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,10 @@
+function stringBasics(){
+    console.log('My dream company is: Google');
+}
+stringBasics()
+var myHobby1 = "cricket";
+var myHobby2 = "singing";
+var myHobby3 = "reading";
+console.log('My hobbies are', myHobby1, myHobby2, myHobby3);
+var total = myHobby1.length + myHobby2.length + myHobby3.length;
+console.log('Total characters in all hobbies are', total);
